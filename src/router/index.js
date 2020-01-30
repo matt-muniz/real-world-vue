1b08b9e5a7dba48d2d0999b6f3c7fe45ec7722f9
Matt Muniz-silva <mattmuniz-silva@Matts-iMac.local>
2020-01-30 12:40:47 -0500

    fixed routing issue

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
     //   import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
-    path: "/event/create",
+    path: "/create",
     name: "event-create",
     component: EventCreate
   }
