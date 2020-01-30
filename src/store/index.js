268303f2b57bc08fbcea459524353ea7cac930b8
Matt Muniz-silva <mattmuniz-silva@Matts-iMac.local>
2020-01-30 11:09:08 -0500

    mutations and actions part 2

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,9 @@ export default new Vuex.Store({
   state: {
     user: { id: "abc123", name: "Matt Silva" },
     categories: ["a", "b", "c", "d"],
-    events: []
+    events: [],
+    eventsTotal: Number,
+    event: {}
   },
   getters: {
     getEventById: state => id => {
@@ -18,6 +20,15 @@ export default new Vuex.Store({
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
+    },
+    SET_EVENT(state, event) {
+      state.event = event;
+    },
+    SET_EVENTTOTAL(state, eventCount) {
+      state.eventsTotal = eventCount;
     },
     SET_USERNAME(state, { id, name }) {
       state.user.name = name;
@@ -29,6 +40,27 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
       });
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(res => {
+          commit("SET_EVENTTOTAL", res.headers["x-total-count"]);
+          commit("SET_EVENTS", res.data);
+        })
+        .catch(error => console.log(error));
+    },
+    fetchEvent({ commit }, id) {
+      const event = this.getters.getEventById(id);
+
+      if (event) {
+        commit("SET_EVENT", event);
+      } else {
+        EventService.getEvent(id)
+          .then(res => {
+            commit("SET_EVENT", res.data);
+          })
+          .catch(error => console.log(error));
+      }
     },
     createUser({ commit }, user) {
       commit("SET_USERNAME", user);
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -31,21 +31,14 @@
 </template>
 
 <script>
-import EventService from "@/services/EventService.js";
-
+import { mapState } from "vuex";
 export default {
   props: ["id"],
-  data() {
-    return {
-      event: {}
-    };
-  },
   created() {
-    EventService.getEvent(this.id)
-      .then(res => {
-        this.event = res.data;
-      })
-      .catch(error => console.log(error));
+    this.$store.dispatch("fetchEvent", this.id);
+  },
+  computed: {
+    ...mapState(["event"])
   }
 };
 </script>
